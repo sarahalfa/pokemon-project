f385ed3e5c9fe3155ccb02499bc2e6794cadfa54
sarahalfa <sarah.alfathlee@city.ac.uk>
2020-04-07 02:48:14 +0100

    defined computer_pokemon variable

--- top_trumps_game.py
+++ top_trumps_game.py
@@ -1,5 +1,4 @@
 import random
- #import the random module to get a random card
 
 print(" ------------------------------------ ")
 print("|                                    |")
@@ -9,14 +8,13 @@ print(" ------------------------------------ ")
 print("")
 
 import requests
-#define the key variable here, add randint, API, and def of pokemon variable
+
 def random_pokemon():
     random_pokemon = random.randint(1, 151)
     url = 'https://pokeapi.co/'.format(random_pokemon)
     response = requests.get(url)
     pokemon = response.json()
 
-#dictionaries list here
     return {
         'name': pokemon['name'],
         'number': pokemon['number'],
@@ -28,24 +26,39 @@ def random_pokemon():
 
     }
 
-#def play, w/ if/else/elif statements for the actual game
+def computer_pokemon():
+    computer_pokemon = random.randint(1, 151)
+    url = 'https://pokeapi.co/'.format(computer_pokemon)
+    response = requests.get(url)
+    pokemon = response.json()
+
+    return {
+            'name': pokemon['name'],
+            'number': pokemon['number'],
+            'ability': pokemon['ability'],
+            'height': pokemon['height'],
+            'weight': pokemon['weight'],
+            'forms': pokemon['forms'],
+            'moves': pokemon['moves'],
+
+        }
 
 def play():
 
-    your_pokemon = random_pokemon()
+    computer_pokemon = random_pokemon
 
     print('You have been given the following pokemon card'.format ['random_pokemon'])
 
     computer_stat: ['object']
     random_pokemon_stat = [random_pokemon['stat_choice'],
-    computer_stat [object] = [random_pokemon['stat_choice'],
-    print('Your opponent has chosen {}'.format(random_pokemon['name']))
+    computer_stat [object] = [computer_pokemon['stat_choice'],
+    print('Your opponent has chosen {}'.format(computer_pokemon['name']))
 
     if computer_stat > random_pokemon_stat:
         print('You Lose!')
-    elif computer_stat < random_pokemon_stat:
+    if computer_stat < random_pokemon_stat:
         print('You Win!')
-    else:
+    elif computer_stat == random_pokemon_stat:
         print('You both win! Its a draw!'),
 
 
